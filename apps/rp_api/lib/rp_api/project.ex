39230125c119d3ddefb5e09e36662dbc134189c8
Cody Roberts <codyjroberts@gmail.com>
2016-12-09 19:55:33 -0600

    add users assoc to project model

--- apps/rp_api/lib/rp_api/project.ex
+++ apps/rp_api/lib/rp_api/project.ex
@@ -1,5 +1,7 @@
 defmodule RpAPI.Project do
   use Ecto.Schema
+  import Ecto.Query
+  alias RpAPI.{User, Repo}
 
   schema "project" do
     field :owner, :string
@@ -8,5 +10,13 @@ defmodule RpAPI.Project do
     has_many :user, RpAPI.User
 
     timestamps
+  end
+
+  def users(project_id) do
+    User
+    |> join(:left, [u], p in assoc(u, :project))
+    |> where([_, p], p.id == ^project_id)
+    |> select([u, _], u)
+    |> Repo.all
   end
 end
